1eec398f45385972807d4d70550fc6d329da1e38
ToxicCalcium <166393308+ToxicCalcium@users.noreply.github.com>
2025-03-01 15:55:51 +0000

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,4 @@
+Capitals = {"India":"New Delhi", "England":"London", "America":"New York", "Scotland":"Edinburgh"}
+print(len(Capitals)) #len is used to find the number of entries inside the dictionary
+print(Capitals.keys()) #.keys is used to list all of the keys in a dictionary
+print(Capitals.values()) #.values is used to list all of the values in a dictionary
