ce63c46321e4951dfea36fd1e8ff543910a42fa8
ToxicCalcium <166393308+ToxicCalcium@users.noreply.github.com>
2025-04-26 16:03:49 +0100

    Add files via upload

--- rectangles.py
+++ rectangles.py
@@ -2,8 +2,17 @@ import pgzrun
 HEIGHT = 400
 WIDTH = 400
 def draw():
-    rect = Rect((200,250),(400,300))
-    rect.center = 200,200
-    screen.draw.rect(rect, "white")
-
+    w = 400
+    h = 100
+    r = 0
+    g = 0
+    b = 255
+    for i in range(20):
+        rect = Rect((200,250),(w,h))
+        rect.center = 200,200
+        screen.draw.rect(rect, (r, g, b))
+        w -= 15
+        h += 15
+        g += 10
+        b -=10
 pgzrun.go()
