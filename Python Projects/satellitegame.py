aaf01ea44e2e6ed27e26cf9d74cdef4138d6d9de
ToxicCalcium <166393308+ToxicCalcium@users.noreply.github.com>
2025-07-19 15:57:39 +0100

    Add files via upload

--- Python Projects/satellitegame.py
+++ Python Projects/satellitegame.py
@@ -6,6 +6,7 @@ WIDTH = 800
 Satellite = []
 Connectors = []
 NumOfSatellites = 7
+NextSatellite = 0
 StartTime = 0
 EndTime = 0
 TotalTime = 0
@@ -13,5 +14,22 @@ TotalTime = 0
 def CreateSatellites():
     global StartTime # global allows variable to be used in other functions
     for i in range(0, NumOfSatellites):
-        Satellites = Actor('satellite.png')
-        
+        SatImg = Actor('satellite.png')
+        SatImg.pos = random.randint(50, WIDTH-50), random.randint(50, HEIGHT-50)
+        Satellite.append(SatImg)
+    StartTime = time.time()
+
+def draw():
+    global TotalTime
+    screen.blit("spacebg", (0,0))
+    Number = 1
+    for SatImg in Satellite:
+        screen.draw.text(str(Number), (SatImg.pos[0], SatImg.pos[1]+20))
+        SatImg.draw()
+        Number = Number + 1
+    
+    for Line in Connectors:
+        screen.draw.line(Connectors[0], Connectors[1], (255, 255, 255))
+    if NextSatellite < NumOfSatellites:
+        Totaltime = time.time() - StartTime
+        screen.draw.text("You Win!", (0, 0))
